7b974b536d3e7a0cf472d56181ed1a499c8c0c17
Ray Liu <aliciaraine1166@gmail.com>
2023-02-07 14:22:30 -0800

    Modify is_sorted function

--- sorted.py
+++ sorted.py
@@ -1,35 +1,31 @@
-
-def is_sorted(List):
+def is_sorted(list):
     """
 
     :param List:
     :return:
-    >>> is_sorted([1, 3, 2])
+    >>> unsorted_list = [5, 3, 2]
+    >>> sorted_list = [1, 2, 3]
+    >>> is_sorted(unsorted_list)
     False
-    >>> is_sorted([1, 2, 3])
+    >>> is_sorted(sorted_list)
     True
     """
     index = 0
-    while index >= len(List -2):
-        if List[index] <= List[index + 1]:
+    while index >= len(list) - 2:
+        if list[index] > list[index + 1]:
+            sorted_check = False
+        else:
             sorted_check = True
-        index += 1
-    else:
-        sorted_check = False
+            index += 1
 
     return sorted_check
-
-
-
-
-
-
 
 
 def main():
     """
     Drive the program.
     """
+
 
 if __name__ == "__main__":
     main()
