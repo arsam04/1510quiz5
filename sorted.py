e017f3c47a4c85ddd3e7ddec4f5b6b2270464c6b
Ray Liu <aliciaraine1166@gmail.com>
2023-02-07 14:09:45 -0800

    Implement is_sorted draft

--- sorted.py
+++ sorted.py
@@ -1,3 +1,31 @@
+
+def is_sorted(List):
+    """
+
+    :param List:
+    :return:
+    >>> is_sorted([1, 3, 2])
+    False
+    >>> is_sorted([1, 2, 3])
+    True
+    """
+    index = 0
+    while index >= len(List -2):
+        if List[index] <= List[index + 1]:
+            sorted_check = True
+        index += 1
+    else:
+        sorted_check = False
+
+    return sorted_check
+
+
+
+
+
+
+
+
 def main():
     """
     Drive the program.
